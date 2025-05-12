0b5ce89e2308db6086ec16a28004fb374c6a0498
002avid <david6espino@gmail.com>
2025-05-12 16:33:00 +0100

    ModifyCompany updated

--- FrontEnd/JavaScript/Company/companyRowClick.js
+++ FrontEnd/JavaScript/Company/companyRowClick.js
@@ -28,7 +28,61 @@ export function initializeRowClickHandlerCompany() {
       const bodyContent = doc.body.innerHTML;
 
       openModal(bodyContent);
+      const modifyBtn = document.getElementById("btnModifyCompany");
+      if(modifyBtn){
+        modifyBtn.addEventListener("click", async () => {
+          response = await fetch(`/modifyCompany`);
+          const html = await response.text();
 
+          const parser = new DOMParser();
+          const doc = parser.parseFromString(html, "text/html");
+          const bodyContent = doc.body.innerHTML;
+          openModal(bodyContent);
+          setTimeout(() => {
+            const modalForm = document.getElementById("company-form");
+            if (!modalForm) return;
+
+            const nameInput = modalForm.querySelector("#company-name");
+            const descInput = modalForm.querySelector("#comapny-description");
+
+            if (nameInput) {
+              nameInput.value = object.name;
+              nameInput.disabled = true;
+            }
+            if (descInput) descInput.value = object.description;
+
+            const form = document.getElementById("category-form");
+            if (form) {
+              form.addEventListener("submit", (event) => {
+                event.preventDefault();
+
+                const description = descInput?.value.trim();
+                if (!description) {
+                  alert("La descripción no puede estar vacía.");
+                  return;
+                }
+
+                fetch("/modify_category", {
+                  method: "PUT",
+                  headers: { "Content-Type": "application/json" },
+                  body: JSON.stringify({
+                    id: object.id,
+                    name: object.name,
+                    description: description
+                  })
+                })
+                    .then(res => {
+                      if (res.ok) {
+                        window.location.href = "/home";
+                      } else {
+                        alert("Error al modificar la categoría.");
+                      }
+                    });
+              });
+            }
+          }, 100);
+        })
+      }
     } catch (err) {
       console.error("Error al cargar el modal:", err);
       Swal.fire({
