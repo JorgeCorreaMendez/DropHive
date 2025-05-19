b0633401ce81f887af0b1c2151f9aaee77538170
Jorge Correa <jorgecorreamen@gmail.com>
2025-05-19 19:33:28 +0100

    Add logging endpoint to retrieve log files with authentication

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+
 from BackEnd.routes.Account import accounts_bp
 from BackEnd.routes.Auth import auth_bp
 from BackEnd.routes.Category import categories_bp
@@ -9,22 +10,29 @@ from BackEnd.routes.Mail import email_bp
 from BackEnd.routes.Privilege import privileges_bp
 from BackEnd.routes.Product import products_bp
 from BackEnd.routes.Register import registro_bp
+from BackEnd.routes.log import log_bp
 from BackEnd.routes.pages import pages_bp
 from BackEnd.utils.flask_mail_methods import init_mail
-from BackEnd.utils.logger import logger
 from BackEnd.utils.sqlalchemy_methods import init_user_db
 
 app = Flask(__name__, template_folder="../FrontEnd/html", static_folder="../FrontEnd")
 app.secret_key = os.getenv("SECRET_KEY")
-app.register_blueprint(auth_bp)
-app.register_blueprint(registro_bp)
-app.register_blueprint(products_bp)
-app.register_blueprint(privileges_bp)
-app.register_blueprint(accounts_bp)
-app.register_blueprint(categories_bp)
-app.register_blueprint(companies_bp)
-app.register_blueprint(email_bp)
-app.register_blueprint(pages_bp)
+
+
+def register_blueprint():
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(registro_bp)
+    app.register_blueprint(products_bp)
+    app.register_blueprint(privileges_bp)
+    app.register_blueprint(accounts_bp)
+    app.register_blueprint(categories_bp)
+    app.register_blueprint(companies_bp)
+    app.register_blueprint(email_bp)
+    app.register_blueprint(pages_bp)
+    app.register_blueprint(log_bp)
+
+
+register_blueprint()
 app.config['APPLICATION_ROOT'] = '/'
 app.json.ensure_ascii = False
 
